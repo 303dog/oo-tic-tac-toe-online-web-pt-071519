a566695c25413d1dba2aeec4ffcf0e4157037793
Paul Beckwith <303dog@gmail.com>
2019-09-04 23:59:42 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,53 @@
+  require "pry"
+
+class TicTacToe
+ 
+ def initialize
+   @board = Array.new(9, " ")
+ end
+
+ WIN_COMBINATIONS = [
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [6,4,2]
+ ]
+ def display_board
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+   puts "-----------"
+   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+   puts "-----------"
+   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+ end
+ 
+ def input_to_index(user_input)
+   user_input = user_input.to_i - 1
+ end
+ #players token AND where the player wants to move
+ def move(index,  token = "X")
+   @board[index] = token
+   
+   #place the token on the board
+ end
+ 
+ 
+ def play
+   display_board
+   puts "Pick a number 1-9"
+   input = gets.chomp
+   idx = self.input_to_index(input)
+   self.move(idx)
+   binding.pry
+ end 
+end
+
+game = TicTacToe.new
+game.play
+
+
+
+
